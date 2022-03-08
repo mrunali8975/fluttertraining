8c90e4a87c31aeb5cc4b50fecbaf2202a7ed659a
Mrunali Ambhore <mrunaliambhore1999@gmail.com>
2022-03-09 02:51:41 +0800

    "Work completed till  day9 "

--- lib/restaurant.dart
+++ lib/restaurant.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:circle_bottom_navigation_bar/circle_bottom_navigation_bar.dart';
+import "package:circle_bottom_navigation_bar/widgets/tab_data.dart";
+import 'package:circle_bottom_navigation_bar/circle_bottom_navigation_bar.dart';
 import 'addcart.dart';
+import 'package:flutter_rating_stars/flutter_rating_stars.dart';
+
 
 Widget size(double mywidth) => SizedBox(width: mywidth);
 
@@ -10,18 +15,223 @@ class RestaurantMenu extends StatefulWidget {
   @override
   State<RestaurantMenu> createState() => _RestaurantMenuState();
 }
+class _MyAppState extends State<MyApp> {
+  double value = 3.5;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Example'),
+        ),
+        body: Center(
+          child: RatingStars(
+            value: value,
+            onValueChanged: (v) {
+              //
+              setState(() {
+                value = v;
+              });
+            },
+            starBuilder: (index, color) => Icon(
+              Icons.ac_unit_outlined,
+              color: color,
+            ),
+            starCount: 5,
+            starSize: 20,
+            valueLabelColor: const Color(0xff9b9b9b),
+            valueLabelTextStyle: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w400,
+                fontStyle: FontStyle.normal,
+                fontSize: 12.0),
+            valueLabelRadius: 10,
+            maxValue: 5,
+            starSpacing: 2,
+            maxValueVisibility: true,
+            valueLabelVisibility: true,
+            animationDuration: Duration(milliseconds: 1000),
+            valueLabelPadding:
+            const EdgeInsets.symmetric(vertical: 1, horizontal: 8),
+            valueLabelMargin: const EdgeInsets.only(right: 8),
+            starOffColor: const Color(0xffe7e8ea),
+            starColor: Colors.yellow,
+          ),
+        ),
+      ),
+    );
+  }
+}
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: const Center(
+          child: Text(
+            'Home',
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
 
-class _RestaurantMenuState extends State<RestaurantMenu> {
+class History extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: const Center(
+          child: Text(
+            'History',
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Search extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: const Center(
+          child: Text(
+            'Search',
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Alarm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: const Center(
+          child: Text(
+            'Alarm ',
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+class _RestaurantMenuState extends State<RestaurantMenu>
+    with TickerProviderStateMixin {
+  int currentPage = 0;
+  final List<Widget> _pages = [
+    Home(),
+    History(),
+    Search(),
+    Alarm(),
+  ];
+  List pizza = [
+    {
+      'name': 'Exotic',
+      'price': '\$139',
+      'desc': 'Chees,olives,jalapenos,tomatoes & onions',
+      'image':
+          'https://images.all-free-download.com/images/graphiclarge/homemade_burger_560254.jpg',
+    },
+    {
+      'name': 'Chilli Cheese Chicken',
+      'price': '\$159',
+      'desc': 'Chicken pepperoni on top of foey chilli cheese',
+      'image':
+          'https://images.all-free-download.com/images/graphiclarge/homemade_burger_560254.jpg',
+    },
+    {
+      'name': 'Potato Wedged',
+      'price': '\$165',
+      'desc': ' Served with cheese  & jalapeno dip',
+      'image':
+          'https://images.all-free-download.com/images/graphiclarge/homemade_burger_560254.jpg',
+    },
+  ];
+  List burger = [
+    {
+      'name': 'Cheese Meltdown',
+      'price': '\$139',
+      'desc':
+          'A BK Original Burger With Cheese oozing Spicy Veg Patty. A Must Try!',
+      'image':'https://image.shutterstock.com/image-photo/vegetarian-burger-260nw-664768828.jpg'
+    },
+    {
+      'image':
+         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8JlJ6IK0bKjqvpcQY-LJWpNBmdf3YWl_Tp7I9ZaEOy485DJbgpdWdJ4OhEacuxmOz_Pk&usqp=CAU',
+      'name': 'Crispy Veg Double Patty + Crispy Veg Double Patty',
+      'price': '\$139',
+      'desc': 'Crispy Veg Double Patty + Crispy Veg Double Patty'
+    },
+    {
+      'image':
+          'https://images.all-free-download.com/images/graphiclarge/homemade_burger_560254.jpg',
+      'name': 'Lite Whopper Jr Veg + Lite Whopper Jr Veg',
+      'price': '\$139',
+      'desc': 'Lite Whopper Jr Veg + Lite Whopper Jr Veg'
+    },
+  ];
+
   TabController? tabController;
   String img =
       'https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Pizza_%281%29.jpg/1200px-Pizza_%281%29.jpg';
   int number = 1;
 
   @override
+  void initState() {
+    tabController = TabController(length: 7, vsync: this);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: CircleBottomNavigationBar(
+        initialSelection: currentPage,
+        circleColor: Colors.purple,
+        activeIconColor: Colors.white,
+        inactiveIconColor: Colors.grey,
+        tabs: [
+          TabData(
+            icon: Icons.home,
+            iconSize: 25,// Optional
+            title: 'Home',// Optional
+            fontSize: 12,// Optional
+          ),
+          TabData(icon: Icons.history),
+          TabData(icon: Icons.search),
+          TabData(icon: Icons.alarm),
+        ],
+        onTabChangedListener: (index) => setState(() => currentPage = index),
+      ),
       body: Column(
         children: [
+
           Padding(
             padding: EdgeInsets.all(10.0),
             child: Stack(
@@ -44,77 +254,102 @@ class _RestaurantMenuState extends State<RestaurantMenu> {
               ],
             ),
           ),
-          Card(
-            elevation: 10.0,
-            color: Colors.black12,
-            margin: EdgeInsets.only(left: 10.0),
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  cardText("PIZZA"),
-                  size(10),
-                  cardText("BURGER"),
-                  size(10),
-                  cardText("COFFIE"),
-                  size(10),
-                  cardText("HOT TEAS"),
-                  size(10),
-                  cardText("DESSERTS"),
-                  size(10),
-                  cardText("LEMONADES"),
-                  size(10),
-                  cardText("LEMONADES")
-                ],
-              ),
+          Expanded(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  height: 50,
+                  child: TabBar(
+                    isScrollable: true,
+                    controller: tabController,
+                    tabs: [
+                      cardText("PIZZA"),
+                      cardText("BURGER"),
+                      cardText("COFFIE"),
+                      cardText("HOT TEAS"),
+                      cardText("DESSERTS"),
+                      cardText("LEMONADES"),
+                      cardText("LEMONADES")
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: TabBarView(
+                    controller: tabController,
+                    children: [
+                      // Container(),
+                      ListView.builder(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemCount: pizza.length,
+                        itemBuilder: (context, index) {
+                          return item(
+                            name: pizza[index]['name'],
+                            Desc: pizza[index]['desc'],
+                            image: data[index]['image'],
+                            price: data[index]['price'],
+                            fav: data[index]['favorite'] ?? false,
+                            onTap: () {
+                              showModalBottomSheet(
+                                  context: context,
+                                  builder: (context) {
+                                    return BottomSheetData(
+                                      img: data[index]['image'],
+                                      name: pizza[index]['name'],
+                                      desc: pizza[index]['desc'],
+                                      price: pizza[index]['price'],
+                                    );
+                                  });
+                            },
+                          );
+                        },
+                      ),
+
+                      ListView.builder(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemCount: pizza.length,
+                        itemBuilder: (context, index) {
+                          return item(
+                            name: burger[index]['name'],
+                            Desc: burger[index]['desc'],
+                            image: data[index]['image'],
+                            price: burger[index]['price'],
+                            fav: data[index]['favorite'] ?? false,
+                            onTap: () {
+                              showModalBottomSheet(
+                                  context: context,
+                                  builder: (context) {
+                                    return BottomSheetData(
+                                      img: burger[index]['image'],
+                                      name: burger[index]['name'],
+                                      desc: burger[index]['desc'],
+                                      price: burger[index]['price'],
+                                    );
+                                  });
+                            },
+                          );
+                        },
+                      ),
+
+                      Container(),
+
+                      Container(),
+
+                      Container(),
+
+                      Container(),
+
+                      Container(),
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
           SizedBox(
             height: 10,
-          ),
-          Expanded(
-            child: ListView.builder(
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              itemCount: data.length,
-              itemBuilder: (context, index) {
-                return item(
-                  name: data[index]['name'],
-                  lastChat: data[index]['lastChat'],
-                  image: data[index]['image'],
-                  fav: data[index]['favorite'] ?? false,
-                  onTap: () {
-                    setState(() {
-                      showModalBottomSheet(
-                          context: context,
-                          builder: (context) {
-                            return BottomSheetData(
-                              img: img,
-                              name: data[index]['name'],
-                              price: data[index]['lastChat'],
-                            );
-                          });
-                    });
-                  },
-                  favorite: () {
-                    setState(() {
-                      data[index]['favorite'] = !data[index]['favorite'];
-                    });
-                  },
-                  moveToNext: () => {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => addCart(
-                                proName: data[index]['name'],
-                                prDesc: data[index]['lastChat'],
-                              )),
-                    )
-                  },
-                );
-              },
-            ),
           ),
         ],
       ),
@@ -139,8 +374,9 @@ class _RestaurantMenuState extends State<RestaurantMenu> {
 
   Widget item({
     String name = '',
-    String lastChat = '',
+    String Desc = '',
     String image = '',
+    String price = '',
     bool fav = false,
     Function()? onTap,
     Function()? favorite,
@@ -150,7 +386,7 @@ class _RestaurantMenuState extends State<RestaurantMenu> {
       onTap: onTap,
       leading: CircleAvatar(
         radius: 40,
-        backgroundImage: NetworkImage(img),
+        backgroundImage: NetworkImage(image),
       ),
       trailing: Column(
         mainAxisSize: MainAxisSize.min,
@@ -173,7 +409,7 @@ class _RestaurantMenuState extends State<RestaurantMenu> {
         style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
       ),
       subtitle: Text(
-        las,
+        price,
         style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
       ),
     );
@@ -182,8 +418,8 @@ class _RestaurantMenuState extends State<RestaurantMenu> {
   List<Map> data = [
     {
       'name': 'ABC',
-      'lastChat': 'Delicious food',
-      'price':'\$500',
+      'Desc': 'Delicious food',
+      'price': '\$500',
       'image':
           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVGHL9r9OucwArH8yO3rEDPryG4V3tSCBw-w&usqp=CAU',
       'onClick': () => print('ABC'),
@@ -192,7 +428,8 @@ class _RestaurantMenuState extends State<RestaurantMenu> {
     },
     {
       'name': 'XYZ',
-      'lastChat': 'Hiii',
+      'Desc': 'Hiii',
+      'price': '\$599',
       'image':
           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVGHL9r9OucwArH8yO3rEDPryG4V3tSCBw-w&usqp=CAU',
       'onClick': () => print('XYZ'),
@@ -201,9 +438,10 @@ class _RestaurantMenuState extends State<RestaurantMenu> {
     },
     {
       'name': 'PQR',
-      'lastChat': 'How are you?',
+      'price': '\$599',
+      'Desc': 'How are you?',
       'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVGHL9r9OucwArH8yO3rEDPryG4V3tSCBw-w&usqp=CAU',
+          'https://images.all-free-download.com/images/graphiclarge/homemade_burger_560254.jpg',
       'onClick': () => print('PQR'),
       'pickTime': '',
       'favorite': false,
@@ -215,8 +453,14 @@ class BottomSheetData extends StatefulWidget {
   String img = '';
   String name = "";
   String price = "";
+  String desc = '';
 
-  BottomSheetData({this.img = '', this.name = '', this.price = '', Key? key})
+  BottomSheetData(
+      {this.desc = '',
+      this.img = '',
+      this.name = '',
+      this.price = '',
+      Key? key})
       : super(key: key);
 
   @override
@@ -296,7 +540,7 @@ class _BottomSheetDataState extends State<BottomSheetData> {
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
                 Text(
-                  "Description fhhh dhhh ",
+                  widget.desc,
                   style: TextStyle(fontSize: 15, color: Colors.black87),
                 ),
                 SizedBox(
@@ -312,20 +556,91 @@ class _BottomSheetDataState extends State<BottomSheetData> {
               ],
             ),
           ),
-          Card(
-            color: Colors.grey,
-            child: Row(
-              children: [
-                Column(
-                  children: [
-                    Text("Cheese"),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Text("\$5")
-                  ],
-                )
-              ],
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Card(
+                color: Colors.grey,
+                child: Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Cheese",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "\$5",
+                        style: TextStyle(fontSize: 20),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.grey,
+                child: Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Cheese",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "\$5",
+                        style: TextStyle(fontSize: 20),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.grey,
+                child: Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Cheese",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "\$5",
+                        style: TextStyle(fontSize: 20),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Center(
+            child: Container(
+              margin: EdgeInsets.all(20.0),
+              width: 310,
+              color: Colors.red,
+              child: TextButton(
+                onPressed: () {
+
+                },
+                child: Text(
+                  "Add Cart",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+              ),
             ),
           )
 
